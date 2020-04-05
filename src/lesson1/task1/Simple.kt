27527476f930bf3e43cc6547dacb3783effca50e
EkSavinova <savika@list.ru>
2020-04-05 10:58:19 +0200

    1st lesson: accountlnThreeYears, angleInRadian, seconds, thirdDigit had been solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -67,7 +67,9 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+    return hours*3600 + minutes*60 + seconds
+}
 
 /**
  * Тривиальная
@@ -76,7 +78,10 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+
+           return (sagenes * 48  + arshins * 3 + vershoks )* 4.445/100
+           }
 
 /**
  * Тривиальная
@@ -125,7 +130,12 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(ini: Int, per: Int): Double {
+    val god1 = ini + per * ini * 0.01
+    val god2 = god1 + per*god1 * 0.01
+
+    return (god2 + god2*per*0.01)
+}
 
 /**
  * Простая
